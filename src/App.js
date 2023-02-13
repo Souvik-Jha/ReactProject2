872b93bcbbb9dca3a6e8a527c58c56aafc9a615c
Souvik Jha <jhasouvik4@gmail.com>
2023-02-13 20:32:10 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React,{useState, Fragment} from 'react';
+import AddUser from "./componeents/Users/AddUser";
+import UsersList from './componeents/Users/UsersList';
+
+function App() {
+  const [usersList,setUsersList]=useState([])
+
+  const addUserHandler=(uName,uAge) =>{
+    setUsersList((prevUsersList) =>{
+      return [...prevUsersList,{name:uName, age:uAge,id: Math.random().toString()}]
+    })
+  }
+
+  return(
+    <Fragment>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </Fragment>
+  )
+}
+
+export default App;
